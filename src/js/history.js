c89bb406f2e11ff817e9d6ca32e0cb77640026ec
M-oplo <markneuise@163.com>
2017-04-18 19:50:03 +0800

    add demo content

--- src/js/history.js
+++ src/js/history.js
@@ -6,7 +6,7 @@ function objRecord(date, content,audio){
 }
 
 var rcd1 = new objRecord(
-			"170420_fin01", 
+			"170420_SFSF01_CN", 
 			
 			"利用 SAP SuccessFactors 可以帮助你自动化并加快员工工时与考勤管理流程：缺勤管理解决方案，使员工能够管理自己的工时相关信息，同时管理人员也能访问这些信息。使用考勤表工具，你能够更全面的了解员工状况，增强工时管理合规性，并提升员工满意度。通过劳动力管理解决方案，你能够满足企业复杂的人力资源需求。",
 			
@@ -14,7 +14,7 @@ var rcd1 = new objRecord(
 );
 			
 var rcd2 = new objRecord(
-			"170420_fin02", 
+			"170420_Ariba01_DE", 
 			
 			"Die Lösungen von Ariba, einem S A P Unternehmen, vereinfachen die Zusammenarbeit bei Aufträgen und Bestellungen, indem Kaufaufträge und auftragsbezogene Dokumente über das Ariba Network übermittelt werden.", 
 			
@@ -22,7 +22,7 @@ var rcd2 = new objRecord(
 );
 			
 var rcd3 = new objRecord(
-			"170420_fin03",
+			"170420_CRM01_EN",
 			
 			"With the SAP CRM Service Manager mobile app, you can extend your SAP CRM and SAP ERP applications to a variety of mobile devices, so you can ensure that your field technicians have all the information and tools they need to execute their service calls quickly and efficiently. Engage customers with personalized experiences across all channels and transform your sales, service, and marketing activities by using on-premise SAP CRM.",
 			
@@ -30,7 +30,7 @@ var rcd3 = new objRecord(
 );
 			
 var rcd4 = new objRecord(
-			"170420_fin04",
+			"170420_SCM01_FR",
 			
 			"Avec les solutions SAP de gestion de la chaîne logistique (SCM), vous répondez aux attentes de plus en plus élevées des clients, vous exécutez des réseaux de distribution de plus en plus complexes et vous atteignez une vitesse de réactivité sans.",
 			
@@ -38,7 +38,7 @@ var rcd4 = new objRecord(
 );
 
 var rcd5 = new objRecord(
-			"170420_fin05",
+			"170420_SAP01_ES",
 			
 			"Como líder de mercado en software de aplicaciones para empresas, SAP ayuda a las organizaciones a combatir los efectos de la complejidad, generar nuevas oportunidades para la innovación y el crecimiento, y mantenerse a la delantera de la competencia.",
 			
@@ -46,7 +46,7 @@ var rcd5 = new objRecord(
 );
 
 var rcd6 = new objRecord(
-			"170420_fin06",
+			"170420_SAP02_IT",
 			
 			"Visita il nostro sito aziendale e scopri news, informazioni per investitori e analisti, risorse per la carriera, la storia di SAP Italia e altro.",
 			
@@ -72,21 +72,48 @@ function testTranslate(date, audio, lang){
 }
 
 var otransrcd1 = new testTranslate(
-				"170420_trans01",
-				"audios/whatstheweatherlike.wav",
-				"en-us"
+				"170420_SFSF02_CN",
+				"audios/chinese-CN_SF_audio-demo.mp3",
+				"zh-cn"
 );
 
 var otransrcd2 = new testTranslate(
-				"170420_trans02",
-				"audios/whatstheweatherlike.wav",
+				"170420_CRM02_EN",
+				"audios/english-US_CRM_audio-demo.mp3",
 				"en-us"
+);
+
+var otransrcd3 = new testTranslate(
+				"170420_SCM02_FR",
+				"audios/french-FR_SCM_audio-demo.mp3",
+				"fr-FR"
+);
+
+var otransrcd4 = new testTranslate(
+				"170420_Ariba02_DE",
+				"audios/German-DE_Ariba_audio-demo.mp3",
+				"de-DE"
+);
+
+var otransrcd5 = new testTranslate(
+				"170420_SAP03_IT",
+				"audios/Italian-IT_SAP_audio-demo.mp3",
+				"it-IT"
+);
+
+var otransrcd6 = new testTranslate(
+				"170420_SAP04_ES",
+				"audios/Spain-ES_SAP_audio-demo.mp3",
+				"es-ES"
 );
 
 var transrcd = new Array();
 transrcd[0] = otransrcd1;
 transrcd[1] = otransrcd2;
-
+transrcd[2] = otransrcd3;
+transrcd[3] = otransrcd4;
+transrcd[4] = otransrcd5;
+transrcd[5] = otransrcd6;
 
 function addItem(rcd, transrcd){
 	var tmp = " ";
