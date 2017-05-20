45b9856c4ae234b6125591d0d9e2aca539d994ce
M-oplo <M-oplo@users.noreply.github.com>
2017-05-20 15:56:01 +0800

    key change

--- src/js/BingSpeech.js
+++ src/js/BingSpeech.js
@@ -13,8 +13,8 @@
 // 
 // LICENSE
 // -------
-// © 2015 Microsoft. All rights reserved.  
-// This document is provided ìas-isî. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
+// ¬© 2015 Microsoft. All rights reserved.  
+// This document is provided ‚Äúas-is‚Äù. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
 // Some examples depicted herein are provided for illustration only and are fictitious.  No real association or connection is intended or should be inferred. 
 // This document does not provide you with any legal rights to any intellectual property in any Microsoft product. You may copy and use this document for your internal, reference purposes. This 
 // document is confidential and proprietary to Microsoft. It is disclosed and can be used only pursuant to a non-disclosure agreement. 
@@ -1171,7 +1171,7 @@ var Bing;
                 this._module = port;
             }
         }
-        NaclClient.kKeyId = "jffoigoenpgbgnhpchggjapfijhffghe";
+        NaclClient.kKeyId = "7bcbc21a24e143f39c987d37026632fa";
         NaclClient.prototype.postMessage = function (arg) {
             if (Bing._window.naclNotInstalled === true) {
                 return;
